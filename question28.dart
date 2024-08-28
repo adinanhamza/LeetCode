cb907bbf32c2a2d9f79cd7069e2d3d520881d64d
adinanhamza <adinanck4557@gmail.com>
2024-08-28 16:58:06 +0530

    Create question28.dart

---
+++ question28.dart
@@ -0,0 +1,36 @@
+28. Find the Index of the First Occurrence in a String
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+// Answer 
+
+class Solution {
+  int strStr(String haystack, String needle) {
+    if (needle.isEmpty) return 0;
+
+    int nLen = needle.length;
+    int hLen = haystack.length;
+
+    for (int i = 0 ; i <= hLen - nLen ; i++){
+        int j;
+
+        for (j = 0 ; j < nLen ; j++){
+            if (haystack[i +j] != needle[j]){
+                break;
+            }
+        }
+
+        if (j ==nLen){
+            return i;
+        }
+    }
+
+    return -1;
+  }
+}
