a48ea24ac5f1aa21a941837981437fa4c9e75563
adinanhamza <adinanck4557@gmail.com>
2024-08-30 09:54:34 +0530

    Create questions709.dart

---
+++ questions709.dart
@@ -0,0 +1,15 @@
+709. To Lower Case
+
+  Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+
+  // Answer
+
+  class Solution {
+  String toLowerCase(String s) {
+    return s.toLowerCase();
+  }
+}
